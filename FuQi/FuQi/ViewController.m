a207cbba3e5987a3b28e4164f4c25feb1cf0fd5d
王晓南 <hahadezuiguo@163.com>
2017-08-18 13:44:17 +0800

    测试提交

--- FuQi/FuQi/ViewController.m
+++ FuQi/FuQi/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"首页";
+    self.view.backgroundColor = [UIColor blackColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
