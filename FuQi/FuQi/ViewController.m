fcab4825a306129c3aae5945e863a0accbd4528b
王晓南 <hahadezuiguo@163.com>
2017-08-18 13:46:02 +0800

    测试测试

--- FuQi/FuQi/ViewController.m
+++ FuQi/FuQi/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     self.title = @"首页";
     self.view.backgroundColor = [UIColor blackColor];
+    NSLog(@"随便说一些东西");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
